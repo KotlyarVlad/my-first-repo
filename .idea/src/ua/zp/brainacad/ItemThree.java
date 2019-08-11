7acfb26db143ce556a216bbc974fc986bfdb6229
Vlad <unforgiving1993@gmail.com>
2019-08-11 15:49:13 +0300

    1.2

--- .idea/src/ua/zp/brainacad/ItemThree.java
+++ .idea/src/ua/zp/brainacad/ItemThree.java
@@ -2,7 +2,7 @@ package ua.zp.brainacad;
 
 public class ItemThree {/** Создать еще один класс ItemThreea.Вывести в столбик слово Hellob.Вывести слово Worldтабуляцией после каждой буквы*/
     public static void main(String[] args) {
-        System.out.println("H\ne\nl\nl\no");
+        System.out.println("H\ne\nl\nl\no\n ");
 
         System.out.println("W\to\tr\tl\td");
     }
